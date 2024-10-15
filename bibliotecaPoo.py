2b8a4690778ab1a65bac1875a8c5b58aec729c2e
GabryelliHelena <gabryelli.design@gmail.com>
2024-10-15 09:57:35 -0300

    init commit

--- bibliotecaPoo.py
+++ bibliotecaPoo.py
@@ -1,9 +1,10 @@
 '''abstração: só pega oq é necessário no momento
 encapsulamento: proteção
 herança: capacidade de uma classe herdar métodos (ações) e atributos (características) de outra classe
-polimorfismo: recebe e reescreve para adaptar'''
+polimorfismo: recebe e reescreve para adaptar
+classe abstrata: foi criada para servir de modelo para outras classes herdarem características '''
 
-'''class pessoa():
+class Pessoa():
     def __init__(self, nome, peso, idade):
         self.nome = nome
         self.peso = peso
@@ -11,7 +12,7 @@ polimorfismo: recebe e reescreve para adaptar'''
         self.comendo = False
         self.andando = False
         self.dormindo = False
-        
+
     def comer(self):
         if self.comendo == False:
             if self.andando == False:
@@ -24,7 +25,7 @@ polimorfismo: recebe e reescreve para adaptar'''
                 print(f"{self.nome} não pode comer pois está andando.")
         else:
             print(f"{self.nome} já está comendo.")
-    
+
     def andar(self):
         if self.andando == False:
             if self.comendo == False:
@@ -34,4 +35,42 @@ polimorfismo: recebe e reescreve para adaptar'''
                 else:
                     print(f"{self.nome} não pode andar pois está dormindo.")
             else:
-                print()'''
+                print()
+
+
+
+class Animal():
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+      print(f"O {self.nome} foi comer...")
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def miar(self):
+        print(f"O {self.nome} foi miando...")
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def latir(self):
+        print(f"O {self.nome} foi latindo...")
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def chiar(self):
+        print(f"O {self.nome} foi chiando...")
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def mugir(self):
+        print(f"A {self.nome} foi mugindo...")
---
+++ mainPoo.py
@@ -0,0 +1,8 @@
+from bibliotecaPoo import Gato, Cachorro, Coelho, Vaca
+
+gato = Gato("Pix", "cinza")
+cachorro = Cachorro("Reginaldo", "caramelo")
+coelho = Coelho("Algodão", "rosa")
+vaca = Vaca("Furiosa", "vermelho")
+
+print(gato.miar(), cachorro.latir(), coelho.chiar(), vaca.mugir())
