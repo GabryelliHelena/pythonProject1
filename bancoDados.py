f50993042713eff052a8a2e61fa7c5d1c0e7c859
GabryelliHelena <gabryelli.design@gmail.com>
2024-10-22 10:07:54 -0300

    init commit

--- bancoDados.py
+++ bancoDados.py
@@ -4,4 +4,18 @@ Dado é a menor parte de um banco de dados, sendo o elemento bruto que é fornec
 Base de dados = Conjunto de arquivos contendo dados relacionados de forma organizada.
 Banco de dados = Arquivo que mantém tudo o que se deseja guardar.
 SGDB (Sistema Gerenciador de Banco de Dados) = software que gerencia os dados do Banco de dados.
+
+3 Fases do projeto de BD:
+Modelo Conceitual = Pensar no conceito de como será preparada a estrutura do banco de dados. É necessário entender de negócio. (Analista de dados)
+É necessário ter visão geral do negócio, tem que saber falar com o usuário e o desenvolvedor.
+Só vão ter lista de regras e funcionalidades.
+
+Modelo lógico = é necessário ter o modelo conceitual. Implementar como uma entidade associativa, transfoprmando algo conceitual em algo prático.
+É necessário definir a chave primária, como um dado elegível (ou único), que identifica a informação.
+Dicionário de dados: Adota a nomenclatura de dados da empresa, padronizando o nome das variáveis para cada tipo de dado. Entidades e atibutos são documentados.
+
+Modelo físico = Define o sistema de gerenciamento de dados, com base nos critérios do projeto. (Não é função do programador, é do DBA)
+DBA (Data Based Administrative) - Adm do banco de dados, especialista em um tipo de estrutura. A certificação é oferecido pelo fabricante do software, precisando fazer os testes que a empresa oferece.
+
+
 '''
