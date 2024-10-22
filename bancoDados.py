0c9546caf0fc08d6c4f80ad3c113d48567eff774
GabryelliHelena <gabryelli.design@gmail.com>
2024-10-22 09:24:20 -0300

    init commit

---
+++ bancoDados.py
@@ -0,0 +1,7 @@
+'''
+Introdução a banco de dados:
+Dado é a menor parte de um banco de dados, sendo o elemento bruto que é fornecido pelo user. O dado por si só não diz nada.
+Base de dados = Conjunto de arquivos contendo dados relacionados de forma organizada.
+Banco de dados = Arquivo que mantém tudo o que se deseja guardar.
+SGDB (Sistema Gerenciador de Banco de Dados) = software que gerencia os dados do Banco de dados.
+'''
